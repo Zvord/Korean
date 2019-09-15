78b3606758d07869d4395d216337cf1bbc23d691
Zvord <37960110+Zvord@users.noreply.github.com>
2019-09-15 17:20:55 +0300

    [~] No autocomplete on inputs [+] Vocabulary training stub

---
+++ Krucible/Pages/VocabularyTraining.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Krucible.Pages
+{
+    public class VocabularyTrainingModel : KruciblePageModel
+    {
+        [BindProperty]
+        public IList<SelectListItem> LessonNumbers { get; set; }
+        [BindProperty]
+        public string SelectedNumber { get; set; }
+        public void OnGet()
+        {
+            LessonNumbers = new List<SelectListItem>();
+            int max = Sogang_1A_Vocabulary.Lessons.Length;
+            for (int i = 1; i <= max; i++)
+                LessonNumbers.Add(new SelectListItem(i.ToString(), i.ToString()));
+            ViewData["xz"] = new SelectList(LessonNumbers);
+            SelectedNumber = "2";
+        }
+        protected override string GetTask()
+        {
+            SelectedNumber = Request.Form["LessonNumber"][0];
+            var Vocab = Sogang_1A_Vocabulary.Lessons[Convert.ToInt32(SelectedNumber) - 1];
+            Random r = new Random();
+            int index = r.Next(Vocab.Count);
+            return Vocab.Keys.ElementAt(index);
+        }
+
+        protected override string GetSolution(string input)
+        {
+            return Sogang_1A_Vocabulary.Test[input];
+        }
+    }
+}
---
+++ Krucible/Sogang_1A_Vocabulary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Krucible
+{
+    using Vocabulary = Dictionary<string, string>;
+    static public class Sogang_1A_Vocabulary
+    {
+        private static Dictionary<string, string> Lesson1;
+        private static Dictionary<string, string> Lesson2;
+        private static Dictionary<string, string> Lesson3;
+        private static Dictionary<string, string> Lesson4;
+        public static Vocabulary Test = new Vocabulary
+        {
+            { "1", "2" },
+            { "3", "4" }
+        };
+
+        public static Vocabulary[] Lessons = new Vocabulary[]
+        {
+            Test,
+            Lesson1,
+            Lesson2,
+            Lesson3,
+            Lesson4
+        };
+    }
+}
